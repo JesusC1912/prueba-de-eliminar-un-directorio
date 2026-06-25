04fcb4099079fa6c945b60955e8e97538f6933d4
JesusC1912 <jesusvillarrealfallas@gmail.com>
2026-06-24 23:44:11 -0600

    prueba

---
+++ prueba de eliminar directorio github/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Punto de entrada de Ciudad Millonaria Java.
+ * Crea el objeto Juego e inicia la ejecucion.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
+}
